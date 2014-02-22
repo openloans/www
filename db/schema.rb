3b74dde6bae4c80e199a1c31a6fd4863c33075e4
Steve Ellis <email@steveell.is>
2014-02-22 17:03:30 -0500

    12 Factor app gem

--- db/schema.rb
+++ db/schema.rb
@@ -15,5 +15,7 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+  enable_extension "dblink"
 
 end
