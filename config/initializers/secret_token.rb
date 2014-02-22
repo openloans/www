a53d3ce11fce3554074f78b87edda68b7c06029a
Steve Danger Ellis <email@steveell.is>
2014-02-22 14:41:26 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenLoans::Application.config.secret_key_base = '17c88c4768d0c195df7c69bc1be83143ebd1cdcd85bda0dbeea7066519a217adf59016627da59ad2ab03a310269f515b4b7b603e5643cc3c9b986ccc6d3b513a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenLoans::Application.config.session_store :cookie_store, key: '_open_loans_session'
